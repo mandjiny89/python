d800740fab7c44ffc1127b8204c8be6851c6ba13
mandjiny <mandjiny89@gmail.com>
2021-05-24 16:26:59 +0100

    removing key

--- project/stock.py
+++ project/stock.py
@@ -4,7 +4,7 @@ import pandas as pd
 from datetime import date
 
 # Setup client
-finnhub_client = finnhub.Client(api_key="c1chfjf48v6vbcpf1ad0")
+finnhub_client = finnhub.Client(api_key="adff")
 
 # Stock candles
 res = finnhub_client.stock_tick('AAPL', '2020-03-25', 500, 0)
@@ -15,6 +15,3 @@ print(res)
 
 
 # # help(finnhub_client.stock_candles)
-
-
-https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=XPEV&interval=60min&apikey=F8Q64V73UOBM1XPX
